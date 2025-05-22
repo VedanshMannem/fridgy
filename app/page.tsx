c3473d3a4f6e37ec9dc5c78e0fea7ddf3e4d00c2
bobRoss21346 <139915934+bobRoss21346@users.noreply.github.com>
2025-05-22 16:47:20 -0500

    updated landing page

--- app/page.tsx
+++ app/page.tsx
@@ -21,22 +21,54 @@ export default function Home() {
   ) : (
     <div>
       {/* <h1 className="flex justify-center ">Never worry about meal planning ever again</h1> */}
-      {/* Insert Figma design here */}
+      <h1 className="flex justify-center items-center mb-4 mt-4">Never worry about cooking again</h1>
       {/* <iframe style={{border: "1px solid rgba(0, 0, 0, 0.1)"}} width="1440" height="1080" src="https://embed.figma.com/design/ZHMGGSx6u0FPjJuDv1jvym/Untitled?node-id=0-1&embed-host=share"></iframe> */}
       <div className="mt-12"></div>
-      <div className="flex justify-center flex-row">
-        <a className="flex flex-col justify-center items-center text-center block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Track your pantry seamlessly</h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">Add/delete pantry items, upload receipts, and track your fridge</p>
-        </a>
-        <a className="flex flex-col justify-center items-center text-center block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Meal planning made easy</h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">Get meal suggestions based on your pantry items</p>
-        </a>
-        <a className="flex flex-col justify-center items-center text-center block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Meal planning made easy</h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">Get meal suggestions based on your pantry items</p>
-        </a>
+      
+      
+
+
+      <div className="flex flex-wrap w-full justify-center flex-row">
+        <div className="card">
+          <p className="heading">  
+            Track your pantry seamlessly
+          </p>
+          <p>
+            <span className="text-green-500 font-bold">Add</span>/
+            <span className="text-red-500 font-bold">Delete</span> pantry items,
+            <span className="text-blue-500 font-bold"> Upload</span> receipts, and
+            <span className="text-yellow-500 font-bold"> Track</span> your fridge
+          </p>
+
+          <p></p>
+        </div>
+
+        <div className="card">
+          <p className="heading">  
+            Meal planning made easy
+          </p>
+          <p>
+            Get <span className="text-orange-500 font-bold">meal suggestions</span> based on your pantry items
+          </p>
+
+          <p></p>
+        </div>
+
+        <div className="card">
+          <p className="heading">  
+            Easily import recipes
+          </p>
+          <p>
+            Import <span className="text-red-500 font-bold">YouTube</span>, 
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#ff0050] to-[#00f2ea] font-bold"> TikTok </span> , and 
+            <span className="text-pink-500 font-bold">Instagram</span>
+            recipes with a single click
+          </p>
+
+          <p></p>
+        </div>
+
+
       </div>
     </div>
   );
